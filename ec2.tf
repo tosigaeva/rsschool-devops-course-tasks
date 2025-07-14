7f8ec8b6009a10feef5cf783cd51179d829defc8
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-07-14 22:44:43 +0200

    feat: migrate to ubuntu

--- ec2.tf
+++ ec2.tf
@@ -187,11 +187,21 @@ resource "aws_instance" "agent_node" {
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu*22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 }
 
